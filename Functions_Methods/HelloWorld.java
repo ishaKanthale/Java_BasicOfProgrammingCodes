2e6ebe13b405356c908f384b8736526b1b8afa04
Isha Kanthale <110197912+ishaKanthale@users.noreply.github.com>
2023-11-22 20:35:11 -0800

    Create HelloWorld.java

---
+++ Functions_Methods/HelloWorld.java
@@ -0,0 +1,15 @@
+package Functions_Methods;
+
+public class HelloWorld {
+
+    public static void printHW() {  //  function definition
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+
+        printHW();  //  function Call
+    }
+}
