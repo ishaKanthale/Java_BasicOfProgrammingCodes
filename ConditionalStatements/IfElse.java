da6b1d3d7d28ec5fe2bdfe688647b84ccce16cd2
Isha Kanthale <110197912+ishaKanthale@users.noreply.github.com>
2023-11-22 20:32:19 -0800

    Create IfElse.java

---
+++ ConditionalStatements/IfElse.java
@@ -0,0 +1,17 @@
+package ConditionalStatements;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        int age = 16;
+        if (age >= 18) {
+            System.out.println("adult : vote, drive");
+        }
+        if (age > 13 && age < 18) {
+            System.out.println("teenager");
+        }else {
+            System.out.println("not adult");
+        }
+    }
+}
